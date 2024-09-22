620310434ac4f290b77e567a0cb4504c40fa5656
Mashruk Kabir <mashrukabirm@gmail.com>
2024-09-22 12:50:33 +0600

    wallpaper script

--- scripts/wall.sh
+++ scripts/wall.sh
@@ -2,13 +2,7 @@
 
 wallpaper_dir="$HOME/Pictures/Wallpapers"
 
-# Check if the wallpaper directory exists
-if [ ! -d "$wallpaper_dir" ]; then
-    echo "Error: Wallpaper directory does not exist."
-    exit 1
-fi
-
-# Function to display wallpaper options using dmenu
+# Display wallpaper list using dmenu
 show_wallpaper_options() {
     # List all image files using dmenu
     wallpapers=$(find "$wallpaper_dir" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -exec basename {} \; | sort)
